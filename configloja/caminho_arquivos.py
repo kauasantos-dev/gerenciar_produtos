bdd6edfa552e182489635c12b9b57f918e0898a0
Kaua Santos <kavillykaua@gmail.com>
2025-11-22 13:01:19 -0300

    refactor: reorganiza módulos de configurações da loja em pacote apropriado

---
+++ configloja/caminho_arquivos.py
@@ -0,0 +1,4 @@
+import os
+
+base_dir = os.path.dirname(__file__)
+save_to = os.path.join(base_dir, 'lista_produtos.json')
---
+++ configloja/gerenciar_arquivos.py
@@ -0,0 +1,17 @@
+import json
+
+from configloja import caminho_arquivos
+
+class AbrirArquivos:
+    @staticmethod
+    def arquivo_r():
+        try:
+            with open(caminho_arquivos.save_to, "r", encoding="utf-8") as file:
+                return json.load(file)
+        except FileNotFoundError:
+            return False
+        
+    @staticmethod
+    def arquivo_w(produtos):
+        with open(caminho_arquivos.save_to, "w", encoding="utf-8") as file:
+            json.dump(produtos, file, indent=2, ensure_ascii=False)
