01bc94aa00e3d6ac43bf32e6547697bf437c4d4d
Kaua Santos <kavillykaua@gmail.com>
2025-12-13 10:20:17 -0300

    refactor: ajusta o caminho de criação do diretório 'produtos', para ser criado corretamente no container do docker
    - O diretório é criado detro da pasta app do container que executa a aplicação
    - Facilita a implementação de volume na execução do container

--- configloja/caminho_arquivos.py
+++ configloja/caminho_arquivos.py
@@ -1,4 +1,5 @@
 import os
 
-base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'produtos'))
+base_dir = os.path.join(os.path.dirname(__file__), '..', 'produtos')
+os.makedirs(base_dir, exist_ok=True)
 save_to = os.path.join(base_dir, 'lista_produtos.json')
